95c2a4cf47468915fb7ac9a5d025fa9a5fb090f4
Thompson Plyler <nealok.g@gmail.com>
2018-10-16 01:27:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-if phrase != phrase.upcase
-  puts "HUH?! SPEAK UP, SONNY!"
-  
+  if phrase != phrase.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "NO, NOT SINCE 1938!"
 end
 
 # Whatever you say to grandma, she should respond with
