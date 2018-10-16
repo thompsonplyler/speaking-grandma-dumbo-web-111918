70d6331388a603a4c38bd39ff23dad3f95cc7c7c
Thompson Plyler <nealok.g@gmail.com>
2018-10-16 02:34:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase = "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     puts "NO, NOT SINCE 1938!"
   end
